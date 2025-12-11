f0161ef01b2c55b7e0066de409c7899c3a540ae5
Anaswasti <anaswasti12@gmail.com>
2025-12-11 12:19:57 -0800

    done

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing >>>
 app.use(
   cors({
-    origin: ["e-learning-client-lime.vercel.app"],
+    origin: ["https://e-learning-client-lime.vercel.app"],
     credentials: true,
   })
 ); 
