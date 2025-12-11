fb93d20eb8ccb27d5a25e72aca318fdf3fd001ba
Anaswasti <anaswasti12@gmail.com>
2025-12-11 12:53:05 -0800

    done

--- build/server.js
+++ build/server.js
@@ -1,18 +1,39 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
+const app_1 = require("./app");
 const cloudinary_1 = require("cloudinary");
-// import { initSocketServer } from "./socketServer";
+const db_1 = __importDefault(require("./utils/db"));
 require("dotenv").config();
-// const server = http.createServer(app);
-// cloudinary confilg >>>
+// Configure Cloudinary from env
 cloudinary_1.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_SECRET_KEY,
 });
-// initSocketServer(server);
-// create server >>>
-// server.listen(process.env.PORT, () => {
-//   console.log(`Server is connected with port ${process.env.PORT}`);
-//   connectDB();
-// });
+// For Vercel serverless functions we must export a handler.
+// Also ensure DB connection is established once per warm instance.
+let dbConnected = false;
+const ensureDB = async () => {
+    if (!dbConnected) {
+        await (0, db_1.default)();
+        dbConnected = true;
+    }
+};
+// Vercel will invoke this function for each request.
+// The Express `app` is callable as a handler: app(req, res)
+async function handler(req, res) {
+    try {
+        await ensureDB();
+        return (0, app_1.app)(req, res);
+    }
+    catch (error) {
+        console.error("Handler error:", error);
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ success: false, message: "Internal Server Error" }));
+    }
+}
--- server.ts
+++ server.ts
@@ -1,22 +1,36 @@
 import { app } from "./app";
 import { v2 as cloudinary } from "cloudinary";
-import http from "http";
 import connectDB from "./utils/db";
-// import { initSocketServer } from "./socketServer";
 require("dotenv").config();
-// const server = http.createServer(app);
 
-// cloudinary confilg >>>
+// Configure Cloudinary from env
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
-// initSocketServer(server);
+// For Vercel serverless functions we must export a handler.
+// Also ensure DB connection is established once per warm instance.
+let dbConnected = false;
 
-// create server >>>
-// server.listen(process.env.PORT, () => {
-//   console.log(`Server is connected with port ${process.env.PORT}`);
-//   connectDB();
-// });
+const ensureDB = async () => {
+  if (!dbConnected) {
+    await connectDB();
+    dbConnected = true;
+  }
+};
+
+// Vercel will invoke this function for each request.
+// The Express `app` is callable as a handler: app(req, res)
+export default async function handler(req: any, res: any) {
+  try {
+    await ensureDB();
+    return app(req, res);
+  } catch (error: any) {
+    console.error("Handler error:", error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ success: false, message: "Internal Server Error" }));
+  }
+}
