035cfff22041819d001316398eb4cf6716d788a9
Anaswasti <anaswasti12@gmail.com>
2025-12-11 12:40:58 -0800

    Fix Vercel deployment: Add vercel.json and API handler

--- server.ts
+++ server.ts
@@ -1,15 +1,22 @@
-require('dotenv').config();
+import { app } from "./app";
+import { v2 as cloudinary } from "cloudinary";
+import http from "http";
+import connectDB from "./utils/db";
+// import { initSocketServer } from "./socketServer";
+require("dotenv").config();
+// const server = http.createServer(app);
 
-import { app } from './app'; 
-import connectDB from './utils/db';
-import { v2 as cloudinary } from 'cloudinary';
-
+// cloudinary confilg >>>
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME as string,
-    api_key: process.env.CLOUD_API_KEY as string,
-    api_secret: process.env.CLOUD_SECRET_KEY as string,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
-module.exports = app;
+// initSocketServer(server);
 
-connectDB().catch(console.error);
+// create server >>>
+// server.listen(process.env.PORT, () => {
+//   console.log(`Server is connected with port ${process.env.PORT}`);
+//   connectDB();
+// });
